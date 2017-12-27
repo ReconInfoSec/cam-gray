2548ad6b2bf7d650b7207ff56c574e9e8d9601c2
Eric Capuano <capuanoe@gmail.com>
2017-12-26 21:00:25 -0600

    Update Auto_update.bat

--- Auto_update.bat
+++ Auto_update.bat
@@ -1,5 +1,6 @@
 @echo on
 cd C:\ProgramData\sysmon\
-@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ReconInfoSec/cam-gray/master/sysmonconfig-export.xml')"
+@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ReconInfoSec/cam-gray/master/sysmonconfig-export.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
 sysmon64 -c sysmonconfig-export.xml
 exit
+
